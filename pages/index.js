ca4336ddc6d4985e2a062904f9be8a3d86686ad7
pavincius <30844451+PAVincius@users.noreply.github.com>
2022-11-29 02:00:46 -0300

    :lipstick: style: Styling CSS

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import TextboxButton from './textBox';
 
 const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>GPT-3 Healthcare Tool | Ey!Icare</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Ey! I care</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>The Future of Healthcare with AI: A smarter, more personalized way to stay healthy.</h2>
           </div>
         </div>
+        <TextboxButton/>
       </div>
       <div className="badge-container grow">
         <a
---
+++ pages/textBox.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TextboxButton() {
+  return (
+    <div className="textbox-button">
+      <input type="text" placeholder="enter text here" />
+      <a href="#" class="button-28">Generate</a>
+    </div>
+  );
+}
+
+export default TextboxButton;
